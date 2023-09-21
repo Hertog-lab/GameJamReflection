916f9b9b105fb4c9ec7831f52f1cc1c9a5719baa
UselessDrunkMeow <timvandergeest030204@gmail.com>
2023-09-21 09:45:09 +0200

    Button en stuff

--- Scripts/Button.gd
+++ Scripts/Button.gd
@@ -1,20 +1,11 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-
-	pass # Replace with function body.
+func _Activate():
 	
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	pass
-
 
 func _on_area_3d_body_entered(body):
 	if body.is_in_group("Projectile"):
 		body.queue_free()
-		print("WAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+		_Activate()
 		pass # Replace with function body.
