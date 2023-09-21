a06be8c5dc1318027077091d690caf155cbad1e0
LukeR2003 <78425989+LukeR2003@users.noreply.github.com>
2023-09-21 21:00:49 +0200

    door update

--- Scripts/DoorScript.gd
+++ Scripts/DoorScript.gd
@@ -1,25 +1,25 @@
 extends StaticBody3D
 
 @export var doorLimit : float
-var active : bool
-
+@export var active : bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	active = false
 	pass # Replace with function body.
 
+func _open_door():
+	active = true
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("OpenDoor"):
-		active = true
-		
+		_open_door()
+	
 	if active == true:
 		self.position += Vector3(0, -2 * delta, 0)
 	
-	print(self.position.y)
-	
 	if self.position.y < doorLimit:
 		self.queue_free()
-		
+	
