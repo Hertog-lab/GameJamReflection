c45efd81c3876ec32f61dcc8342da88f99a2d2b3
hertog <hertog@fsfe.org>
2023-09-21 13:20:20 +0200

    PlayerMovement finished :3

--- Scripts/Player/PlayerStates/Move.gd
+++ Scripts/Player/PlayerStates/Move.gd
@@ -1,10 +1,12 @@
 extends Node
 @onready var player = get_node(^"../..")
+@export var speed = 10
 
 func StateReady():
 	print("Current state = Move")
 
 func StateProcess():
 	var input_direction = Input.get_vector("Left", "Right", "Forward", "Backward")
+	player.velocity = Vector3(input_direction.x, 0, input_direction.y) * speed
 	if input_direction == Vector2(0,0):
 		player.SwapState(get_node(^"../Idle"))
